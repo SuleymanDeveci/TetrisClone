ed149b84e763803215724ece17f1e8a20aecc980
UsturaBlade <suleymandeveci1907@gmail.com>
2024-09-08 01:17:38 +0300

    HardDrop and Rotation Added

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour
@@ -8,12 +6,14 @@ public class Block : MonoBehaviour
     public Vector3Int Position { get; private set; }
     public Vector3Int[] Cells { get; private set; }
     public TetrominoData TData { get; private set; }
+    public int RotationIndex {  get; private set; }
 
     public void Initialize(GameBoard board, Vector3Int position, TetrominoData tData)
     {
         Board = board;
         Position = position;
         TData = tData;
+        RotationIndex = 0;
 
        /*  --------------------Bir alt satýrdaki kodun uzun hali
         if(Cells == null)
@@ -33,13 +33,32 @@ public class Block : MonoBehaviour
     {
         Board.Clear(this);
 
-        if(Input.GetKeyDown(KeyCode.A))
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            HandleRotation(-1);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            HandleRotation(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
         {
             HandleMovement(Vector2Int.left);
         }
         else if(Input.GetKeyDown(KeyCode.D))
         { 
             HandleMovement(Vector2Int.right);
+        }
+
+        if(Input.GetKeyDown(KeyCode.S)) 
+        { 
+            HandleMovement(Vector2Int.down);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            HandleHardDrop();
         }
 
         Board.Set(this);
@@ -59,6 +78,47 @@ public class Block : MonoBehaviour
         }
 
         return isValid;
+    }
+
+    public void HandleRotation(int direction)
+    {
+        int originalRotation = RotationIndex;
+        RotationIndex = ExtensionMethods.Wrap(RotationIndex + direction, 0, 4);
+        ApplyRotationMatrix(direction);
+    }
+
+    private void ApplyRotationMatrix(int direction)
+    {
+        for(int i = 0; i< Cells.Length; ++i) 
+        {
+            Vector3Int cell = Cells[i];
+
+            int x, y;
+
+            switch (TData.tetromino)
+            {
+                case Tetromino.Letter_I:
+                case Tetromino.Letter_O:
+                    x = Mathf.CeilToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.CeilToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    break;
+
+                default:
+                    x = Mathf.RoundToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.RoundToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    break;
+            }
+
+            Cells[i] = new Vector3Int(x, y, 0);
+        }
+    }
+
+    private void HandleHardDrop()
+    {
+        while(HandleMovement(Vector2Int.down))
+        {
+            continue;
+        }
     }
 
 }
